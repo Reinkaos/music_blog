5a475bd74736cf485448e68557ac759e692afd66
Reinkaos <rj.matthews23@gmail.com>
2016-10-29 11:16:04 +1300

    remove user id

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20161028215001) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
